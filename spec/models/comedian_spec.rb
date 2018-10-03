58717a44212e7d76ff118ded662ee08f2d1dc91f
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-03 16:41:18 -0600

    adds avgage for stats bar

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Comedian, type: :model do
     it {should validate_presence_of(:age)}
 
   end
+
+  describe 'Class Methods' do
+    it '.avg_age' do
+      comic_1 = Comedian.create(name: "Jim Bob", age: 48, city: "Denver")
+      comic_2 = Comedian.create(name: "Sally Jo", age: 22, city: "Vegas")
+      comic_3 = Comedian.create(name: "Steve Schmeve", age: 23, city: "Vegas")
+      avg_age = Comedian.avg_age
+
+      expect(avg_age).to eq(31)
+    end
+  end
 end
