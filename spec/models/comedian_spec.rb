f401b45dfb695f6c896828a207e509f8807f9765
Nick Lindeberg <lindebergnick@gmail.com>
2018-10-06 11:52:55 -0600

    updates seed and adds some styling

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe Comedian, type: :model do
   describe 'Validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:age)}
+    it {should validate_presence_of(:city)}
 
   end
 
